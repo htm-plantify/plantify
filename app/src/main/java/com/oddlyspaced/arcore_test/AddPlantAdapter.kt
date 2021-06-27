d8816665e6a7b1cc15b52bfe215a63a5b39235f6
oddlyspaced <oddlyspaced1@gmail.com>
2021-06-27 12:56:05 +0530

    AddPlantAdapter: Setup base adapter

---
+++ app/src/main/java/com/oddlyspaced/arcore_test/AddPlantAdapter.kt
@@ -0,0 +1,33 @@
+package com.oddlyspaced.arcore_test
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.oddlyspaced.arcore_test.databinding.ItemAddPlantBinding
+
+class AddPlantAdapter(private val plants: ArrayList<Plant>): RecyclerView.Adapter<AddPlantAdapter.AddPlantViewHolder>() {
+
+    var onItemClick: (Plant) -> Unit = {}
+
+    class AddPlantViewHolder(private val binding: ItemAddPlantBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Plant) {
+            binding.txAddPlantName.text = data.name
+            binding.txAddPlantImage.setImageResource(data.image)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddPlantViewHolder {
+        return AddPlantViewHolder(ItemAddPlantBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: AddPlantViewHolder, position: Int) {
+        holder.bind(plants[position])
+        holder.itemView.setOnClickListener {
+            onItemClick(plants[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return plants.size
+    }
+}
